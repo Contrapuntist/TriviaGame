c316449c702da8a871d116ab8d0fe3fd09c4f243
Contrapuntist <contrapuntist@gmail.com>
2017-05-30 19:57:09 -0500

    added basic text to appear on screen

--- javascript/app.js
+++ javascript/app.js
@@ -9,14 +9,32 @@ $(document).ready(function() {
 		} 
 	} 
 
-	var counters = {
+	var gameTracker = {
 		correctAnswersCount: 0, 
 		wrongAnswersCount: 0, 
 		notAnsweredCount: 0,
+		userChoice: null,
 
 	} 
 
 
 	$("#question").text(triviaQuestions.question1.q); 
+	$("#answer-1").text(triviaQuestions.question1.rtAnswer);
+	$("#answer-2").text(triviaQuestions.question1.wrAnswer);
+	console.log(gameTracker.userChoice);  
+
+	$("answer-1").click(function() {
+		/* Act on the event */
+		gameTracker.userChoice = triviaQuestions.question1.rtAnswer; 
+		console.log(gameTracker.userChoice);
+	}); 
+
+
+	$("answer-2").on('click',function() {
+		/* Act on the event */
+		gameTracker.userChoice = triviaQuestions.question1.wrAnswer;
+		console.log(gameTracker.userChoice);
+	});
+
 
 });
