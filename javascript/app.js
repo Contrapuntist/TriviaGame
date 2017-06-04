ccc0da0039c3d3bd5887908ee42f036cfffebb41
Contrapuntist <contrapuntist@gmail.com>
2017-06-04 15:07:11 -0500

    adding timer code

--- javascript/app.js
+++ javascript/app.js
@@ -36,9 +36,7 @@ $(document).ready(function() {
  				li.append("<input type='radio' name='q-answer' id='answer-" + a + "' value=" + triviaQuestions.question1.ansOptions[i] + "'>" + triviaQuestions.question1.ansOptions[i] + "</input>"); 
  				a++;
  			}    
-			$('#start-button').remove(); 
-			var test = $('#answer-2').val();
-			console.log(test);
+			$('#start-button').remove(); 		
 		},  
  
 		gameStart: function () { 
@@ -47,6 +45,52 @@ $(document).ready(function() {
 			});
 		},
 
+
+		// game timer
+		gametime: 30,
+
+		timerReset: function() {
+
+		    this.gametime = 30;
+
+		    // DONE: Change the "display" div to "00:00."
+		    
+		    $('#timecontainer').html("<h2>Time left: <span id='gametimer'></span></h2>")
+		    $('#timecontainer').html('#gametimer', '00:30');
+		  },
+		
+	  	countdown: function() {
+
+		    // DONE: increment time by 1, remember we cant use "this" here.
+		    gameTracker.gametime--;
+
+		    // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
+		    //       and save the result in a variable.
+		    var converted = gameTracker.timeConverter(gameTracker.time);
+		    console.log(converted);
+
+		    // DONE: Use the variable we just created to show the converted time in the "display" div.
+		    $("#timecontainer").html('#gametimer', converted);
+		},
+		
+		timeConverter: function(t) {
+
+		    var minutes = Math.floor(t / 60);
+		    var seconds = t - (minutes * 60);
+
+		    if (seconds < 10) {
+		      seconds = "0" + seconds;
+		    }
+
+		    if (minutes === 0) {
+		      minutes = "00";
+		    }
+		    else if (minutes < 10) {
+		      minutes = "0" + minutes;
+		    }
+
+		    return minutes + ":" + seconds;
+		}
 	}; 
  	
 	//$("#question").text(triviaQuestions.question1.q); 
@@ -56,8 +100,8 @@ $(document).ready(function() {
 	//$("#answer-4").text(triviaQuestions.question1.ansOptions[3]); 
 
 	gameTracker.gameStart();
-	
-	$("#answer-1").click(function() {
+	// event propogation, bubbling up
+	$("#answers").on('click', '#answer-1', function() {
 		/* Act on the event */
 		console.log("radio clicked");
 		gameTracker.userChoice = $('#answer-1').val();
@@ -65,44 +109,31 @@ $(document).ready(function() {
 	}); 
 
 
-	$("#answer-2").on('click',function() {
+	$("#answers").on('click', '#answers-2', function() {
 		/* Act on the event */ 
 		console.log("radio clicked");
 		gameTracker.userChoice = $('#answer-2').val();
-		//if ( gameTracker.userChoice )
-
 		console.log(gameTracker.userChoice);
-		//console.log(alert("Right answer"));
 	});  
 
-	$("#answer-2").on('click',function() {
+	$("#answers").on('click', '#answer-3', function() {
 		/* Act on the event */
 		gameTracker.userChoice = triviaQuestions.question1.wrAnswer;
 		console.log(gameTracker.userChoice);
 	}); 
 
-	$("#answer-2").on('click',function() {
+	$("#answers").on('click', '#answer-4',function() {
 		/* Act on the event */
 		gameTracker.userChoice = triviaQuestions.question1.wrAnswer;
 		console.log(gameTracker.userChoice);
 	}); 
+
 
 	//method to gauge object length
 	//console.log(Object.keys(gameTracker).length);
 
-	function pickRandomProperty(obj) {
-	    //var result;
-	    var count = 0;
-	    for (var prop in obj)
-	        if (Math.random() < 1/++count)
-	        	console.log(prop);
-	           gameTracker.triviaQSelect = prop.q; 
-	    console.log(gameTracker.triviaQSelect);
-	}
+	//function gameState(u) { if };
 
-	//function gameState(u) { if }
-
-	pickRandomProperty(triviaQuestions);
 	console.log(gameTracker.triviaQSelect); 
 
 });
