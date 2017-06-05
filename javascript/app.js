942e11f6a1944ef6d2b4860c6ec2cb6c6617a7ad
Contrapuntist <contrapuntist@gmail.com>
2017-06-04 22:47:10 -0500

    added html remove and replace code

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function() { 
 
+	// https://www.uselessdaily.com/news/40-amazing-facts-about-the-tv-series-the-big-bang-theory-list/ 
+
 	var triviaQuestions = [
 		{
 			q: "Who wrote the Lord of The Rings trilogy?",
@@ -22,6 +24,8 @@ $(document).ready(function() {
 	]; 
 
 	var gameTracker = {
+		
+		//variables used throughout game
 		correctAnswersCount: 0, 
 		wrongAnswersCount: 0, 
 		notAnsweredCount: 0,
@@ -33,8 +37,8 @@ $(document).ready(function() {
 		},
 
 		gameQuestionPrint: function() {
-			this.triviaQSelect = this.randomizer(0, triviaQuestions.length) 
-			console.log(this.triviaQSelect)
+			this.triviaQSelect = this.randomizer(0, triviaQuestions.length); 
+			console.log(this.triviaQSelect);
 			$("#question").text(triviaQuestions[this.triviaQSelect].q); 
 			var a = 1;
  			for (var i = 0; i < triviaQuestions[this.triviaQSelect].ansOptions.length; i++) {
@@ -44,33 +48,49 @@ $(document).ready(function() {
  					triviaQuestions[this.triviaQSelect].ansOptions[i] + "</input>");
  				a++;
  			}    
-			$('#start-button').remove(); 		
+			$('#start-button').remove();
+			$('#submitbtn').html('<button>Submit</button>'); 		
 		},  
  
 		gameStart: function () { 
 			$('#start-button').click(function(){
 				gameTracker.gameQuestionPrint(); 
-				gameTracker.timerReset();
+				gameTracker.timerReset(this.gametime);
 			});
 			console.log(gameTracker.userChoice);
 		},
 
-		gameState: function (a) { 
-			if (a === triviaQuestions[0].rtAnswer) { 
-				correctAnswersCount++;
+		answerSubmit: function (a) { 
+			if (a === triviaQuestions[this.triviaQSelect].rtAnswer) { 
+				gameTracker.correctAnswersCount++;
+				console.log(gameTracker.correctAnswersCount + ' questions right');
+				gameTracker.stop();
+				$('#game').empty();
+				this.scoreUpdate();
+			} else { 
+				wrongAnswersCount++;
+				gameTracker.stop();
+				this.scoreUpdate();
 			}
-		},
+		}, 
 
+		// next window with new html
+		scoreUpdate: function() { 
+			$('#game').html('<h2>Correct answers: ' + this.correctAnswersCount + '<h2>');
+			$('#game').append('<h2>Wrong answers: ' + this.wrongAnswersCount + '<h2>');
+			$('game').append('<h2>Questions not answered: ' + this.notAnsweredCount + '<h2>') 
+		}, 
 
 		// game timer
 		gametime: 30,
 		gameinterval: null,
-		timerReset: function() {
+		
+		timerReset: function(time) {
 
-		    gameTracker.gametime = 30;
+		    //gameTracker.gametime = 30;
 		    		    
 		    $('#timecontainer').html("<h2>Time left: <span id='gametimer'></span> seconds </h2>");
-		    $('#gametimer').text('30'); 
+		    $('#gametimer').text(time); 
 		    
 		    gameTracker.gameinterval = setInterval(gameTracker.countdown, 1000); 
 		  },
@@ -79,22 +99,18 @@ $(document).ready(function() {
 
 		    // reduce time
 		    gameTracker.gametime--;
-
 		    $("#gametimer").html(gameTracker.gametime); 
-
 		    if (gameTracker.gametime === 0 ) {
-
 		    	gameTracker.stop();
 		    }
 			
-		    console.log( 'time is ' + gameTracker.gametime);
+		    //console.log( 'time is ' + gameTracker.gametime);
 		},
 		
 		stop: function() {
     		// DONE: Use clearInterval to stop the count here and set the clock to not be running.
     		clearInterval(gameTracker.gameinterval);
-  			console.log('stop reached');
-  		},
+   		},
 	}; 
  	
 	//$("#question").text(triviaQuestions.question1.q); 
@@ -133,5 +149,10 @@ $(document).ready(function() {
 		gameTracker.userChoice = $('#answer-4').val();;
 		console.log(gameTracker.userChoice);
 	}); 
+
+	$('#submitbtn').on('click', function(){ 
+		gameTracker.answerSubmit(gameTracker.userChoice);
+		console.log('submit clicked');
+	});
 
 });
