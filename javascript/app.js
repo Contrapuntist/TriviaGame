0b760c538dd3ede8f8ea11c201ae116068764f8f
Contrapuntist <contrapuntist@gmail.com>
2017-06-04 21:03:03 -0500

    timer fixed

--- javascript/app.js
+++ javascript/app.js
@@ -1,25 +1,25 @@
 $(document).ready(function() { 
 
-	var triviaQuestions = {
-		question1: {
+	var triviaQuestions = [
+		{
 			q: "Who wrote the Lord of The Rings trilogy?",
 			ansOptions: ['Michael Crichton', 'J.R.R. Tolkien', 'Philip K. Dick', 'George R.R. Martin'],
 			rtAnswer: "J.R.R. Tolkien",
 
 		}, 
 
-		question2: {
+		{
 			q: "What Marvel character is big and green with a bad temper?",
 			ansOptions: ['Thor', 'Hulk', 'Iron Man', 'Captain America'],
 			rtAnswer: 'Hulk',
 		}, 
 
-		question3: {	
+		{	
 			q: "When was the last time the Chicago Bears won the Super Bowl?", 
 			ansOptions: ['1980', '2006', '1988', '1985'], 
 			rtAnswer: '1985',
 		}, 
-	}; 
+	]; 
 
 	var gameTracker = {
 		correctAnswersCount: 0, 
@@ -29,11 +29,11 @@ $(document).ready(function() {
 		triviaQSelect: null,
 
 		gameQuestionPrint: function() {
-			$("#question").text(triviaQuestions.question1.q); 
+			$("#question").text(triviaQuestions[0].q); 
 			var a = 1;
- 			for (var i = 0; i < triviaQuestions.question1.ansOptions.length; i++) {
+ 			for (var i = 0; i < triviaQuestions[0].ansOptions.length; i++) {
  				var li = $("<li>").appendTo("#answers");
- 				li.append("<input type='radio' name='q-answer' id='answer-" + a + "' value=" + triviaQuestions.question1.ansOptions[i] + "'>" + triviaQuestions.question1.ansOptions[i] + "</input>"); 
+ 				li.append("<input type='radio' name='q-answer' id='answer-" + a + "' value='" + triviaQuestions[0].ansOptions[i] + "'>" + triviaQuestions[0].ansOptions[i] + "</input>"); 
  				a++;
  			}    
 			$('#start-button').remove(); 		
@@ -42,55 +42,51 @@ $(document).ready(function() {
 		gameStart: function () { 
 			$('#start-button').click(function(){
 				gameTracker.gameQuestionPrint(); 
+				gameTracker.timerReset();
 			});
+			console.log(gameTracker.userChoice);
+		},
+
+		gameState: function (a) { 
+			if (a === triviaQuestions[0].rtAnswer) { 
+				correctAnswersCount++;
+			}
 		},
 
 
 		// game timer
 		gametime: 30,
-
+		gameinterval: null,
 		timerReset: function() {
 
-		    this.gametime = 30;
-
-		    // DONE: Change the "display" div to "00:00."
+		    gameTracker.gametime = 30;
+		    		    
+		    $('#timecontainer').html("<h2>Time left: <span id='gametimer'></span> seconds </h2>");
+		    $('#gametimer').text('30'); 
 		    
-		    $('#timecontainer').html("<h2>Time left: <span id='gametimer'></span></h2>")
-		    $('#timecontainer').html('#gametimer', '00:30');
+		    gameTracker.gameinterval = setInterval(gameTracker.countdown, 1000); 
 		  },
 		
 	  	countdown: function() {
 
-		    // DONE: increment time by 1, remember we cant use "this" here.
+		    // reduce time
 		    gameTracker.gametime--;
 
-		    // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
-		    //       and save the result in a variable.
-		    var converted = gameTracker.timeConverter(gameTracker.time);
-		    console.log(converted);
+		    $("#gametimer").html(gameTracker.gametime); 
 
-		    // DONE: Use the variable we just created to show the converted time in the "display" div.
-		    $("#timecontainer").html('#gametimer', converted);
+		    if (gameTracker.gametime === 0 ) {
+
+		    	gameTracker.stop();
+		    }
+			
+		    console.log( 'time is ' + gameTracker.gametime);
 		},
 		
-		timeConverter: function(t) {
-
-		    var minutes = Math.floor(t / 60);
-		    var seconds = t - (minutes * 60);
-
-		    if (seconds < 10) {
-		      seconds = "0" + seconds;
-		    }
-
-		    if (minutes === 0) {
-		      minutes = "00";
-		    }
-		    else if (minutes < 10) {
-		      minutes = "0" + minutes;
-		    }
-
-		    return minutes + ":" + seconds;
-		}
+		stop: function() {
+    		// DONE: Use clearInterval to stop the count here and set the clock to not be running.
+    		clearInterval(gameTracker.gameinterval);
+  			console.log('stop reached');
+  		},
 	}; 
  	
 	//$("#question").text(triviaQuestions.question1.q); 
@@ -100,6 +96,7 @@ $(document).ready(function() {
 	//$("#answer-4").text(triviaQuestions.question1.ansOptions[3]); 
 
 	gameTracker.gameStart();
+
 	// event propogation, bubbling up
 	$("#answers").on('click', '#answer-1', function() {
 		/* Act on the event */
@@ -108,32 +105,25 @@ $(document).ready(function() {
 		console.log(gameTracker.userChoice);
 	}); 
 
-
-	$("#answers").on('click', '#answers-2', function() {
-		/* Act on the event */ 
+	$("#answers").on('click', '#answer-2', function() {
+		/* Act on the event */
 		console.log("radio clicked");
 		gameTracker.userChoice = $('#answer-2').val();
 		console.log(gameTracker.userChoice);
-	});  
+	});
 
 	$("#answers").on('click', '#answer-3', function() {
 		/* Act on the event */
-		gameTracker.userChoice = triviaQuestions.question1.wrAnswer;
+		console.log("radio clicked");
+		gameTracker.userChoice = $('#answer-3').val();;
 		console.log(gameTracker.userChoice);
 	}); 
 
 	$("#answers").on('click', '#answer-4',function() {
 		/* Act on the event */
-		gameTracker.userChoice = triviaQuestions.question1.wrAnswer;
+		console.log("radio clicked");
+		gameTracker.userChoice = $('#answer-4').val();;
 		console.log(gameTracker.userChoice);
 	}); 
-
-
-	//method to gauge object length
-	//console.log(Object.keys(gameTracker).length);
-
-	//function gameState(u) { if };
-
-	console.log(gameTracker.triviaQSelect); 
 
 });
